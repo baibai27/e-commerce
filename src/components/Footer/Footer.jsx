e8402e52930ad8e62bc3d9bb29cdf05f867f5f60
Nicole Bai <baijingwen1127@gmail.com>
2023-05-11 16:26:40 -0400

    First commit, completed navbar and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Cateogries</h1>
+          <span>Women</span>
+          <span>Women</span>
+          <span>Children</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            quae hic dolorem quo ratione maxime placeat doloribus consequuntur
+            rem sapiente?
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quos
+            id doloribus! Incidunt alias quia porro maiores fugiat quidem sunt
+            impedit placeat, expedita labore quam.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">GuiizeStore</span>
+          <span className="copyright">Copyright</span>
+        </div>
+        <div className="right">
+          <img
+            src="https://store-cdn.arduino.cc/uni/wysiwyg/Payment_Options.jpg"
+            width={400}
+            height={50}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img
+              src="https://png.pngitem.com/pimgs/s/131-1313870_canada-flag-png-small-canada-flag-icon-transparent.png"
+              alt="canada flag"
+              width={20}
+              height={15}
+            />
+            <KeyboardArrowDownRoundedIcon />
+          </div>
+          <div className="item">
+            <span>CAD</span>
+            <KeyboardArrowDownRoundedIcon />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+
+        <div className="center">
+          <Link className="link" to="/">
+            GUIIZESTORE
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+
+          <div className="icons">
+            <SearchIcon />
+            <FavoriteBorderIcon />
+            <PersonOutlineIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
